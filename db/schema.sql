62f35bb86c2534b11081c6287de5f9ef9d0ba02e
Jacob Adamson <jakeladamson@gmail.com>
2021-08-30 21:12:51 -0600

    writing psuedo code

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+-- drop db
+-- create db
+
+-- use db
+
+-- create table dept
+
+-- create table role
+
+-- create table employee
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- insert into dept
+-- values
+
+-- insert into role
+-- values
+
+-- insert into employee 
+-- values
