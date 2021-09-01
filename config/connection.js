b0da0d5ce6e58902bea2217f6764a6d2a39090cd
Jacob Adamson <jakeladamson@gmail.com>
2021-08-31 21:06:23 -0600

    not getting connection error, menu and app title still buggy

--- config/connection.js
+++ config/connection.js
@@ -9,9 +9,9 @@ const dbConnect = mysql.createConnection({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: "localhost",
-  dialect: "mysql2",
-  port: 3306,
+  // host: "localhost",
+  // dialect: "mysql2",
+  // port: 3306,
 });
 console.log("Connected to database");
 
