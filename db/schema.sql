75e351ec66d5f68b86d806f8c9066eb7c47ad2ec
Jacob Adamson <jakeladamson@gmail.com>
2021-08-31 22:20:38 -0600

    worked on schema and seeds sql files from ex info

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,27 @@
--- drop db
--- create db
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
+USE company_db;
 
--- use db
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40) NOT NULL
+);
 
--- create table dept
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_ID INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
--- create table role
-
--- create table employee
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(40) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+);
